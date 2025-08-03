03f82a532b6455630bd2cc6be6be89c1f5781a0c
James Antony Francis Manley <jimmux@users.noreply.github.com>
2025-08-03 16:46:40 +1000

    More modules

--- module4/Rationals/Task/src/rationals/Rational.kt
+++ module4/Rationals/Task/src/rationals/Rational.kt
@@ -1,17 +1,26 @@
 package rationals
 
 import java.math.BigInteger
+import kotlin.toBigInteger
 
-class Rational(val numerator: Long, val denominator: Long) {
-    operator fun plus(other: Rational): Rational = Rational(
-        this.numerator + other.numerator,
-        this.denominator
-    )
+class Rational(val numerator: BigInteger, val denominator: BigInteger) {
+    operator fun plus(other: Rational): Rational {
+        val (a, b) = this.common(other)
 
-    operator fun minus(other: Rational): Rational = Rational(
-        this.numerator - other.numerator,
-        this.denominator
-    )
+        return Rational(
+            a.numerator + b.numerator,
+            a.denominator
+        )
+    }
+
+    operator fun minus(other: Rational): Rational {
+        val (a, b) = this.common(other)
+
+        return Rational(
+            a.numerator - b.numerator,
+            a.denominator
+        )
+    }
 
     operator fun times(other: Rational): Rational = Rational(
         this.numerator * other.numerator,
@@ -19,35 +28,77 @@ class Rational(val numerator: Long, val denominator: Long) {
     )
 
     operator fun div(other: Rational): Rational = Rational(
-        this.numerator * other.numerator,
-        this.denominator * other.denominator
+        this.numerator * other.denominator,
+        this.denominator * other.numerator
     )
 
     operator fun unaryMinus(): Rational = Rational(
-        -this.numerator,
-        this.denominator
+        -numerator,
+        denominator
     )
 
-    operator fun compareTo(other: Rational): Int =
-        (this.numerator * other.denominator).compareTo(other.numerator * this.denominator)
+    override operator fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Rational) return false
+
+        return this.compareTo(other) == 0
+    }
+
+    operator fun compareTo(other: Rational): Int {
+        val (a, b) = common(other)
+        return (a.numerator).compareTo(b.numerator)
+    }
 
     operator fun rangeTo(other: Rational) = Pair(this, other)
 
-    override fun toString() = "${this.numerator}/${this.denominator}"
+    override fun toString(): String {
+        val normalized = this.normalized()
+
+        if (normalized.denominator == BigInteger.ONE) {
+            return normalized.numerator.toString()
+        }
+
+        return "${normalized.numerator}/${normalized.denominator}"
+    }
+
+    fun normalized(): Rational {
+        var gcd = denominator.gcd(numerator)
+        if (denominator < BigInteger.ZERO) {
+            gcd *= -BigInteger.ONE
+        }
+        return Rational(numerator / gcd, denominator / gcd)
+    }
+
+    fun common(other: Rational): Pair<Rational, Rational> {
+        val d = this.denominator * other.denominator
+        return Pair(
+            Rational(this.numerator * other.denominator, d),
+            Rational(other.numerator * this.denominator, d)
+        )
+    }
+
+    override fun hashCode(): Int {
+        var result = numerator.hashCode()
+        result = 31 * result + denominator.hashCode()
+        return result
+    }
 }
 
 operator fun Pair<Rational, Rational>.contains(r: Rational): Boolean =
     this.first <= r && r <= this.second
 
 infix fun Int.divBy(denominator: Int) =
-    Rational(this.toLong(), denominator.toLong())
+    Rational(this.toBigInteger(), denominator.toBigInteger())
 infix fun Long.divBy(denominator: Long): Rational =
-    Rational(this, denominator)
+    Rational(this.toBigInteger(), denominator.toBigInteger())
 infix fun BigInteger.divBy(denominator: BigInteger): Rational =
-    Rational(this.toLong(), denominator.toLong())
+    Rational(this, denominator)
 
 fun String.toRational(): Rational {
-    val (numerator, denominator) = this.split("/").map(String::toLong)
+    val parts = this.split("/").map(String::toBigInteger)
+    val numerator = parts.getOrElse(0, { BigInteger.ZERO})
+    val denominator = parts.getOrElse(1, {BigInteger.ONE})
+
     return Rational(numerator, denominator)
 }
 
